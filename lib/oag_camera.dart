2c15341c03cbd79b868cad11cb747826d45a3fce
Orlys Azaharez Garcia <orlys.azaharez@lacoloniamedical.com>
2023-10-12 12:18:05 -0400

    export camera lens direction

--- lib/oag_camera.dart
+++ lib/oag_camera.dart
@@ -1,5 +1,6 @@
 library oag_camera;
 
+export 'package:camera/camera.dart' show CameraLensDirection;
 export './model/camera_configuration.dart';
 export './model/camera_item.dart';
 export './model/camera_item_type.dart';
